b262f8e6a83d90dd8050f7d4461b9eb13cf4a7df
77web <info@77-web.com>
2022-01-26 22:32:03 +0900

    added Document prototype: FacebookFeedAd, GoogleResponsiveSearchAd

---
+++ src/Document/FacebookFeedAd.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Document;
+
+class FacebookFeedAd
+{
+    private ?string $headline;
+    private ?string $mainText;
+    private ?string $linkUrl;
+    private ?string $imageUrl;
+}
---
+++ src/Document/GoogleResponsiveSearchAd.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Document;
+
+class GoogleResponsiveSearchAd
+{
+    private array $headlines = [];
+    private array $descriptions = [];
+    private ?string $finalUrl;
+    private ?string $path1;
+    private ?string $path2;
+}
